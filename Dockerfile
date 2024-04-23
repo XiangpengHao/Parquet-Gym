0eec73319ab60ec1a051a573332338aef614ae87
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2024-04-23 15:14:53 +0000

    update more

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,22 @@ ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get install -y \
     curl \
     build-essential \
+    python3 \
     gcc
 
+
+# Install Rust
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Install uv
+RUN curl -LsSf https://astral.sh/uv/install.sh | sh
+
 ENV PATH="/root/.cargo/bin:${PATH}"
 
-CMD ["bash"]
+WORKDIR /app
+
+COPY . /app
+
+RUN uv venv && uv pip install -r requirements.txt 
+
+CMD [".venv/bin/python", "benchmark.py"]
--- benchmark.py
+++ benchmark.py
@@ -1,17 +1,23 @@
 import toml
 import subprocess
 import os
+import json
+import time
+import pyarrow as pa
+import pyarrow.parquet as pq
+import datetime
 
 
-def run_command(command, cwd) -> bool:
+def run_command(command, cwd) -> float | None:
     try:
-        # Run the command and wait for it to complete
+        start_time = time.time()
         subprocess.run(command, cwd=cwd, shell=True, check=True)
-        return True
+        elapsed_time = time.time() - start_time
+        return elapsed_time
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while executing: {command}")
         print(f"Error: {e}")
-        return False
+        return None
 
 
 def build_reader(cwd, reader):
@@ -23,23 +29,76 @@ def build_reader(cwd, reader):
             break
 
 
-def benchmark(readers, files):
+def benchmark_one(reader, workload) -> dict | None:
     workspace_dir = os.path.dirname(os.path.realpath(__file__))
+    working_dir = os.path.join(workspace_dir, reader["working_dir"])
+
+    executable_path = os.path.join(working_dir, reader["bin"])
+    workload_path = os.path.join(workspace_dir, workload["path"])
+
+    elapsed = run_command(f"{executable_path} {workload_path}", working_dir)
+    if not elapsed:
+        return None
+
+    print(
+        f"Reader: {reader['name']}, File: {workload['path']}, Elapsed time: {elapsed}s"
+    )
+
+    return {
+        "reader": reader,
+        "workload": workload,
+        "result": {
+            "elapsed": elapsed,
+        },
+    }
+
+
+def benchmark(readers, workloads):
+    workspace_dir = os.path.dirname(os.path.realpath(__file__))
+    results = []
+
     for r in readers:
         working_dir = os.path.join(workspace_dir, r["working_dir"])
         build_reader(working_dir, r)
-        for f in files:
-            executable = os.path.join(working_dir, r["bin"])
-            workload_file = os.path.join(workspace_dir, f)
+        for f in workloads:
+            r = benchmark_one(r, f)
+            results.append(r)
+    return results
 
-            ok = run_command(f"{executable} {workload_file}", working_dir)
-            if not ok:
-                break
+
+def save_results(results, dst_dir):
+    now = datetime.datetime.now()
+    date_str = now.strftime("%Y-%m-%d")
+    time_str = now.strftime("%H-%M")
+
+    date_directory = os.path.join(dst_dir, date_str)
+
+    if not os.path.exists(date_directory):
+        os.makedirs(date_directory)
+
+    json_file = os.path.join(date_directory, f"{time_str}-results.json")
+    try:
+        with open(json_file, "w") as f:
+            json.dump(results, f, indent=4)
+            print(f"Json results saved to {json_file}")
+    except Exception as e:
+        print(f"Failed to save the json file: {e}")
+
+    try:
+        parquet_file = os.path.join(date_directory, f"{time_str}-results.parquet")
+        table = pa.Table.from_pylist(results)
+        pq.write_table(table, parquet_file)
+        print(f"Parquet results saved to {parquet_file}")
+    except Exception as e:
+        print(f"Failed to save the parquet file: {e}")
 
 
 if __name__ == "__main__":
     with open("config.toml") as f:
         config = toml.load(f)
 
-    files = [x["path"] for x in config["workloads"]]
-    benchmark(readers=config["readers"], files=files)
+    workspace_dir = os.path.dirname(os.path.realpath(__file__))
+
+    output_dir = os.path.join(workspace_dir, "results")
+    results = benchmark(readers=config["readers"], workloads=config["workloads"])
+    save_results(results, output_dir)
