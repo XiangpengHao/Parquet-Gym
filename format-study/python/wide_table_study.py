a3f91bb2cc2b5cd11880d13641937529edfae3bf
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2024-06-04 15:31:28 -0400

    update analysis

--- format-study/python/wide_table_study.py
+++ format-study/python/wide_table_study.py
@@ -62,72 +62,88 @@ def load_data(input_dir):
     df['stats'] = df.apply(lambda x: stats_type_from_file_name(x['file_name']), axis=1)
     return df
 
-def plot(input_dir, output_dir):
-    df = load_data(input_dir) 
-    fig, (ax2, ax1) = plt.subplots(2, 2, figsize=(10, 8))
-    fig.subplots_adjust(hspace=0.4)
-    hue_order = ['none', 'chunk', 'page']
+hue_order = ['none', 'chunk', 'page']
+
+def plot_line(df, ax1, ax2):
     sns.lineplot(data=df, x='column_cnt', y='metadata_end_to_end_load_time_nanos', hue='stats', 
-                 ax=ax1[0], hue_order=hue_order, markers=True, style='stats', dashes=False)
-    ax1[0].legend(title='Stats level', frameon=False)
-    ax1[0].set_xlabel('Number of columns')
-    ax1[0].set_ylabel('Metadata decode time (ms)')
-    ax1[0].set_yscale('log')
-    ax1[0].set_xscale('log')
-    ax1[0].set_title('Metadata decode time (trend)')
-    ax1[0].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
+                 ax=ax1, hue_order=hue_order, markers=True, style='stats', dashes=False)
+    ax1.legend(title='Stats level', frameon=False)
+    ax1.set_xlabel('Number of columns')
+    ax1.set_ylabel('Metadata decode time (ms)')
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
+    ax1.set_title('Metadata decode time (trend)')
+    ax1.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
 
     sns.lineplot(data=df, x='column_cnt', y='metadata_len', hue='stats', 
-                 ax=ax1[1], hue_order=hue_order, markers=True, style='stats', dashes=False)
-    ax1[1].legend(title='Stats level', frameon=False)
-    ax1[1].set_xlabel('Number of columns')
-    ax1[1].set_ylabel('Metadata size (MB)')
-    ax1[1].set_xscale('log')
-    ax1[1].set_yscale('log')
-    ax1[1].set_title('Metadata size (trend)')
-    ax1[1].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
+                 ax=ax2, hue_order=hue_order, markers=True, style='stats', dashes=False)
+    ax2.legend(title='Stats level', frameon=False)
+    ax2.set_xlabel('Number of columns')
+    ax2.set_ylabel('Metadata size (MB)')
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
+    ax2.set_title('Metadata size (trend)')
+    ax2.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
 
+
+def plot_bar(df, ax1, ax2):
     column_subset = [1_000, 2_000, 5_000, 10_000]
     df_subset = df[df['column_cnt'].isin(column_subset)]
 
     palette = sns.color_palette()[:3]
 
     sns.barplot(data=df_subset, x='column_cnt', y='metadata_end_to_end_load_time_nanos', hue='stats',
-                hue_order=hue_order, ax=ax2[0], palette=palette)
+                hue_order=hue_order, ax=ax1, palette=palette)
     sns.barplot(data=df_subset, x='column_cnt', y='metadata_len', hue='stats', 
-                hue_order= hue_order,ax=ax2[1])
-    ax2[0].set_xlabel('Number of columns')
-    ax2[1].set_xlabel('Number of columns')
-    ax2[1].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
-    ax2[0].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
+                hue_order= hue_order,ax=ax2)
+    ax1.set_xlabel('Number of columns')
+    ax2.set_xlabel('Number of columns')
+    ax2.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
+    ax1.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: '{:.0f}'.format(x/1e6)))
 
     darker_palette = [sns.set_hls_values(c, l=.3) for c in palette]
     sns.barplot(data=df_subset, x='column_cnt', y='schema_build_time_nanos', hue='stats',
-                hue_order=hue_order, ax=ax2[0], palette=darker_palette)
+                hue_order=hue_order, ax=ax1, palette=darker_palette)
 
-    ax2[0].set_ylabel('Metadata decode time (ms)')
-    ax2[1].set_ylabel('Metadata size (MB)')
+    ax1.set_ylabel('Metadata decode time (ms)')
+    ax2.set_ylabel('Metadata size (MB)')
 
-    h, l = ax2[0].get_legend_handles_labels()
-    ax2[0].legend(title='Stats level', labels=l[:3], handles=h[:3], frameon=False)
+    h, l = ax1.get_legend_handles_labels()
+    ax1.legend(title='Stats level', labels=l[:3], handles=h[:3], frameon=False)
 
     
-    ax2[0].annotate('Thrift decode', xy=(2, 20_000_000), xytext=(0.3, 0.4), 
+    ax1.annotate('Thrift decode', xy=(2, 20_000_000), xytext=(0.3, 0.4), 
                     textcoords='axes fraction', arrowprops=dict(facecolor='black', arrowstyle='->'))
 
-    ax2[0].annotate('Schema build', xy=(2, 1_000_000), xytext=(0.2, 0.2), 
+    ax1.annotate('Schema build', xy=(2, 1_000_000), xytext=(0.2, 0.2), 
                     textcoords='axes fraction', arrowprops=dict(facecolor='black', arrowstyle='->'))
 
-    ax2[1].legend(title='Stats level', frameon=False)
-    ax2[0].set_title('Metadata decode time')
-    ax2[1].set_title('Metadata size')
+    ax2.legend(title='Stats level', frameon=False)
+    ax1.set_title('Metadata decode time')
+    ax2.set_title('Metadata size')
 
+
+def plot_all(input_dir, output_dir):
+    df = load_data(input_dir) 
+    fig, (ax2, ax1) = plt.subplots(2, 2, figsize=(10, 8))
+    fig.subplots_adjust(hspace=0.4)
+
+    plot_line(df, ax1[0], ax1[1])
+    plot_bar(df, ax2[0], ax2[1])
 
     fig.savefig(os.path.join(output_dir, "metadata_decode_time.png"), dpi=300)
 
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    plot_line(df, ax1, ax2)
+    fig.savefig(os.path.join(output_dir, "metadata_decode_trend.png"), dpi=300)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    plot_bar(df, ax1, ax2)
+    fig.savefig(os.path.join(output_dir, "metadata_decode_bar.png"), dpi=300)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate and benchmark Parquet files")
-    parser.add_argument("action", choices=["generate", "benchmark", "plot"], help="Action to perform")
+    parser.add_argument("action", choices=["generate", "benchmark", "plot", "plot_trend"], help="Action to perform")
     parser.add_argument("--output_dir", help="Directory for the output files")
     parser.add_argument("--input_dir", help="Directory for the input files", default="")
 
@@ -142,4 +158,4 @@ if __name__ == "__main__":
     elif args.action == "benchmark":
         benchmark(args.input_dir, output_dir)
     elif args.action == "plot":
-        plot(args.input_dir, output_dir)
+        plot_all(args.input_dir, output_dir)
